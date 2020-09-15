f967eca303cd7e99c82173d2bc5d627b620bf816
Jonathan <darmon101@gmail.com>
2020-09-15 14:14:42 +0300

    Changes made and components added

--- src/app/school/school.component.ts
+++ src/app/school/school.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { StudentService } from '../services/student.service';
+import { CourseService } from '../services/course.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-school',
@@ -8,10 +10,43 @@ import { StudentService } from '../services/student.service';
 })
 export class SchoolComponent implements OnInit {
 
-  constructor(private studentService: StudentService) { }
+  constructor(private studentService: StudentService,
+              private courseService: CourseService,
+              private router: Router) { }
+
+  students: Array<any>;
+  courses: Array<any>;
+  container: string;
+    studentClick(student): any {
+      console.log(student);
+      this.router.navigate(['/school/student']);
+    }
+
+    courseClick(course): any {
+      console.log(course);
+      this.router.navigate(['/school/course']);
+    }
+
+    addCourse(): any {
+      this.router.navigate(['/school/edit-course']);
+    }
+
+    addStudent(): any {
+      this.router.navigate(['/school/edit-student']);
+    }
+
+    routerStatus(): boolean {
+      return this.router.url === '/school';
+    }
 
   ngOnInit(): void {
-    this.studentService.getStudents().subscribe(res => console.log(res));
+    this.studentService.getStudents().subscribe(res => {
+      this.students = res;
+    });
+
+    this.courseService.getCourses().subscribe(res => {
+      this.courses = res;
+    });
   }
 
 }
